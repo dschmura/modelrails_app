29edf1bf8db402edee1acfff24112f7c370ffc83
Dave Chmura <dschmura@humbledaisy.com>
2022-09-28 13:57:35 -0400

    ADD: Flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,14 @@ module ApplicationHelper
     def skip_links
       sanitize @skip_links.collect { |skip_link| link_to "#{skip_link[1]}", "##{skip_link[0]}", class: "skip-to-content-link" }.join(" <span class='sr-only' > or </span>")
     end
+
+    def tailwind_classes_for(flash_type)
+      {
+        notice: "bg-green-600 border-l-4 border-green-700 text-green-50 ",
+        error:   "bg-red-800 border-l-4 border-red-900 text-red-50",
+        alert:   "bg-red-800 border-l-4 border-red-900 text-red-50",
+      }.stringify_keys[flash_type.to_s] || flash_type.to_s
+    end
     
+
 end
