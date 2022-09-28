12d01dcde83632df768e96b4de70009dd3114150
Dave Chmura <dschmura@humbledaisy.com>
2022-09-28 15:20:06 -0400

    ADD: Gravatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar       
+
+  def image_url
+     GravatarHelper.gravatar_url_for(email)
+  end
+
 end
