d21145092b74070f017bf2105cb6e109eac7331b
Dave Chmura <dschmura@humbledaisy.com>
2022-09-29 10:01:47 -0400

    FIX: Error when cancelling an account

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+
   scope controller: :static_pages do
     get :about
     get :contact
