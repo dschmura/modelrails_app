15bf399c7d9cb723536c3cc03d41d4d8967891e3
David Chmura <dschmura@humbledaisy.com>
2023-03-15 15:38:10 -0400

    REFACTOR: reformat per standard

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,12 +20,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:shibboleth, :google_oauth2]
+    :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:shibboleth, :google_oauth2]
   has_many :omni_auth_services, dependent: :destroy
-  has_one_attached :avatar       
+  has_one_attached :avatar
 
   def image_url
-     GravatarHelper.gravatar_url_for(email)
+    GravatarHelper.gravatar_url_for(email)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
   end
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  
+
   scope controller: :static_pages do
     get :about
     get :contact
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "static_pages#index"
-  resources "feedbacks", only: [:create]  
+  resources "feedbacks", only: [:create]
 end
