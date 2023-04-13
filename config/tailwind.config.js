bd1101790f771128db8a9aaab61ed7cf987d6bc5
David Chmura <dschmura@humbledaisy.com>
2023-04-13 14:16:20 -0400

    ADD: Umich colors [umich-maze and umich-blue]

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,12 +5,21 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
+    './public/documentation/**/*.{erb,haml,html,slim,md}',
     './vendor/javascript/tailwindcss-stimulus-components.js'
+
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        umich: {
+          maze: '#FFCF01',
+          blue: '#00274c',
+        },
+    
       },
     },
   },
