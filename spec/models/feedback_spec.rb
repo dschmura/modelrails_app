8a6e495c164585971c0d830b80d023ea38891513
David Chmura <dschmura@humbledaisy.com>
2023-06-05 09:48:25 -0400

    ADD: Spec files

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe Feedback, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "invalid submissions" do
+
+    it "is not valid without a full name" do
+      feedback = Feedback.new(full_name: nil)
+      expect(feedback).to_not be_valid
+    end
+
+    it "is not valid without a valid email" do
+      feedback = Feedback.new(email: nil)
+      expect(feedback).to_not be_valid
+    end
+
+    it "is not valid without a topic" do
+      feedback = Feedback.new(topic: nil)
+      expect(feedback).to_not be_valid
+    end
+
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,24 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "validations" do
+
+    it "is not valid without an email" do
+      user = User.new(email: nil)
+      expect(user).to_not be_valid
+    end
+
+    it "is not valid without a password" do
+      user = User.new(password: nil)
+      expect(user).to_not be_valid
+    end
+
+    it "is not valid without a password_confirmation" do
+      user = User.new(password_confirmation: nil)
+      expect(user).to_not be_valid
+    end
+
+
+  end
 end
