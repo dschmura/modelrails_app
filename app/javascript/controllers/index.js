327fc19fdccc06871fa5aaa9a65730f686b20753
David Chmura <dschmura@humbledaisy.com>
2023-09-20 16:29:01 -0400

    REFACTOR: replace Chris Oliver's tailwindcss-stimulus-components with the broader https://www.stimulus-components.com/docs/stimulus-notification

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,17 +6,11 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
+import Dropdown from 'stimulus-dropdown'
+import Notification from 'stimulus-notification'
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
 
-// Import and register all TailwindCSS Components
-import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
-application.register('alert', Alert)
-application.register('autosave', Autosave)
 application.register('dropdown', Dropdown)
-application.register('modal', Modal)
-application.register('tabs', Tabs)
-application.register('popover', Popover)
-application.register('toggle', Toggle)
-application.register('slideover', Slideover)
+application.register('notification', Notification)
