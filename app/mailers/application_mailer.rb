6b7d67dcf4b097eb5582fc3240b36498c0ed8460
David Chmura <dschmura@humbledaisy.com>
2023-11-21 14:03:32 -0500

    REFACTOR: Mailers into own view directory.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "t('feedback_mailer.from')"
   layout "mailer"
+  prepend_view_path "app/views/mailers"
+  
 end
