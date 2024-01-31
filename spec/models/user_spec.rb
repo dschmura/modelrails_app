1f11faf63c20570668ee6e97fea173a7ec3a944e
Dave Chmura <dschmura@humbledaisy.com>
2024-01-31 07:11:24 -0500

    UPDATE: Bundle update

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,8 +19,6 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-
   describe "validations" do
     it "is not valid without an email" do
       user = User.new(email: nil)
--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -1,18 +1,17 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Errors", type: :request do
   describe "GET /not_found" do
     it "returns http success" do
       get "/errors/not_found"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(404)
     end
   end
 
   describe "GET /internal_server_error" do
     it "returns http success" do
       get "/errors/internal_server_error"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(500)
     end
   end
-
 end
